861d255bc974c04106cd94044c240a5d0ce97ae8
Erickson Cruz <erickson@adapsense.com>
2020-02-14 13:28:12 +0800

    circle detection improved through search localization

--- source/main.py
+++ source/main.py
@@ -1,257 +1,136 @@
 #!/usr/bin/env python
-"""
- Copyright (C) 2018-2019 Intel Corporation
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-"""
-
-from __future__ import print_function
+# import the necessary packages
+import numpy as np
 import sys
 import os
 from argparse import ArgumentParser, SUPPRESS
+import logging as log
 import cv2
 import time
-import logging as log
-import numpy as np
-from openvino.inference_engine import IENetwork, IEPlugin
-
 
 def build_argparser():
     parser = ArgumentParser(add_help=False)
     args = parser.add_argument_group('Options')
     args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
-    args.add_argument("-m", "--model", help="Required. Path to an .xml file with a trained model.",
-                      required=True, type=str)
     args.add_argument("-i", "--input",
                       help="Required. Path to video file or image. 'cam' for capturing video stream from camera",
                       required=True, type=str)
-    args.add_argument("-l", "--cpu_extension",
-                      help="Optional. Required for CPU custom layers. Absolute path to a shared library with the "
-                           "kernels implementations.", type=str, default=None)
-    args.add_argument("-pp", "--plugin_dir", help="Optional. Path to a plugin folder", type=str, default=None)
-    args.add_argument("-d", "--device",
-                      help="Optional. Specify the target device to infer on; CPU, GPU, FPGA, HDDL or MYRIAD is "
-                           "acceptable. The demo will look for a suitable plugin for device specified. "
-                           "Default value is CPU", default="CPU", type=str)
-    args.add_argument("--labels", help="Optional. Path to labels mapping file", default=None, type=str)
-    args.add_argument("-pt", "--prob_threshold", help="Optional. Probability threshold for detections filtering",
-                      default=0.5, type=float)
 
     return parser
-
 
 def main():
     log.basicConfig(format="[ %(levelname)s ] %(message)s", level=log.INFO, stream=sys.stdout)
     args = build_argparser().parse_args()
-    model_xml = args.model
-    model_bin = os.path.splitext(model_xml)[0] + ".bin"
-    # Plugin initialization for specified device and load extensions library if specified
-    log.info("Initializing plugin for {} device...".format(args.device))
-    plugin = IEPlugin(device=args.device, plugin_dirs=args.plugin_dir)
-    if args.cpu_extension and 'CPU' in args.device:
-        plugin.add_cpu_extension(args.cpu_extension)
-    # Read IR
-    log.info("Reading IR...")
-    net = IENetwork(model=model_xml, weights=model_bin)
 
-    if plugin.device == "CPU":
-        supported_layers = plugin.get_supported_layers(net)
-        not_supported_layers = [l for l in net.layers.keys() if l not in supported_layers]
-        if len(not_supported_layers) != 0:
-            log.error("Following layers are not supported by the plugin for specified device {}:\n {}".
-                      format(plugin.device, ', '.join(not_supported_layers)))
-            log.error("Please try to specify cpu extensions library path in demo's command line parameters using -l "
-                      "or --cpu_extension command line argument")
-            sys.exit(1)
-    assert len(net.inputs.keys()) == 1, "Demo supports only single input topologies"
-    assert len(net.outputs) == 1, "Demo supports only single output topologies"
-    input_blob = next(iter(net.inputs))
-    out_blob = next(iter(net.outputs))
-    log.info("Loading IR to the plugin...")
-    exec_net = plugin.load(network=net, num_requests=2)
-    # Read and pre-process input image
-    n, c, h, w = net.inputs[input_blob].shape
-    del net
     if args.input == 'cam':
         input_stream = '/dev/video0'
     else:
         input_stream = args.input
         assert os.path.isfile(args.input), "Specified input file doesn't exist"
-    if args.labels:
-        with open(args.labels, 'r') as f:
-            labels_map = [x.strip() for x in f]
-    else:
-        labels_map = None
 
-    cap = cv2.VideoCapture(input_stream)
+    cap = cv2.VideoCapture(input_stream) # Set Capture Device, in case of a USB Webcam try 1, or give -1 to get a list of available devices
 
-    cur_request_id = 0
-    next_request_id = 1
+    #Set Width and Height 
+    # cap.set(3,1280)
+    # cap.set(4,720)
 
-    log.info("Starting inference in async mode...")
-    log.info("To switch between sync and async modes press Tab button")
-    log.info("To stop the demo execution press Esc button")
-    is_async_mode = True
-    render_time = 0
-    ret, frame = cap.read()
+    # The above step is to set the Resolution of the Video. The default is 640x480.
+    # This example works with a Resolution of 640x480.
+
+    kernel = np.ones((3,3),np.uint8)
     
-    '''Erickson's code insertion starts here'''
+    #must find a way to localize the location for HoughCircles
     
-    if args.input != 'cam':
-        task_start = time.time()
+    y = 0;
+    x = 0;
+    r = 0;
     
-        processTimeStart = time.time();
-        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        processTimeEnd = time.time();
-        print("Gray Conversion Time: {}".format(processTimeEnd - processTimeStart))
-        
-        
-        kernel_size = 5;
-        processTimeStart = time.time();
-        blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
-        processTimeEnd = time.time();
-        print("Gaussian Blur Time: {}".format(processTimeEnd - processTimeStart))
-        
-        
-        processTimeStart = time.time();
-        ret, gray = cv2.threshold(gray,50,255,cv2.THRESH_BINARY)
-        processTimeEnd = time.time();
-        print("Threshold Time: {}".format(processTimeEnd - processTimeStart))
-        
-        #kernel = np.ones((5,5),np.uint8)
-        #gray = cv2.dilate(gray,kernel,iterations = 3)
-        
-        cv2.imwrite("gray.jpg", gray)
-        
-        low_threshold = 50
-        high_threshold = 150
-        edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
-        
-        rho = 1  # distance resolution in pixels of the Hough grid
-        theta = np.pi / 180  # angular resolution in radians of the Hough grid
-        threshold = 100  # minimum number of votes (intersections in Hough grid cell)
-        min_line_length = 10  # minimum number of pixels making up a line
-        max_line_gap = 20  # maximum gap in pixels between connectable line segments
-        line_image = np.copy(frame) * 0  # creating a blank to draw lines on
-
-        # Run Hough on edge detected image
-        # Output "lines" is an array containing endpoints of detected line segments
-        
-        processTimeStart = time.time();
-        lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),min_line_length, max_line_gap)
-        processTimeEnd = time.time();
-        print("HoughLinesP Time: {}".format(processTimeEnd - processTimeStart))
-        
-        
-        perimeter=[]
-
-        for line in lines:
-            for x1,y1,x2,y2 in line:
-                 perimeter.append(abs(x2-x1) + abs(y2-y1))
-                 cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
-            
-        '''ind = np.argmax(perimeter)
-        
-        for x1,y1,x2,y2 in lines[ind]:
-            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)'''
-                
-        lines_edges = cv2.addWeighted(frame, 0.8, line_image, 1, 0)
-        
-        cv2.imwrite("sample_output.jpg", lines_edges)
-        
-        task_end = time.time()
-        task_time = task_end - task_start
-        print("Task Time: {}".format(task_time))
-        
-    '''end of insertion code'''
-
-    print("To close the application, press 'CTRL+C' or any key with focus on the output window")
     while cap.isOpened():
-        if is_async_mode:
-            ret, next_frame = cap.read()
-        else:
+       	# Capture frame-by-frame
+       	if args.input == 'cam':
             ret, frame = cap.read()
-        if not ret:
-            break
-        initial_w = cap.get(3)
-        initial_h = cap.get(4)
-        # Main sync point:
-        # in the truly Async mode we start the NEXT infer request, while waiting for the CURRENT to complete
-        # in the regular mode we start the CURRENT request and immediately wait for it's completion
-        inf_start = time.time()
-        if is_async_mode:
-            in_frame = cv2.resize(next_frame, (w, h))
-            in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-            in_frame = in_frame.reshape((n, c, h, w))
-            exec_net.start_async(request_id=next_request_id, inputs={input_blob: in_frame})
         else:
-            in_frame = cv2.resize(frame, (w, h))
-            in_frame = in_frame.transpose((2, 0, 1))  # Change data layout from HWC to CHW
-            in_frame = in_frame.reshape((n, c, h, w))
-            exec_net.start_async(request_id=cur_request_id, inputs={input_blob: in_frame})
-        if exec_net.requests[cur_request_id].wait(-1) == 0:
-            inf_end = time.time()
-            det_time = inf_end - inf_start
+            frame = cv2.imread(input_stream)
+            
+        Yloc = y - 2*r
+        Xloc = x - 2*r
 
-            # Parse detection results of the current request
-            res = exec_net.requests[cur_request_id].outputs[out_blob]
-            for obj in res[0][0]:
-                # Draw only objects when probability more than specified threshold
-                if obj[2] > args.prob_threshold:
-                    xmin = int(obj[3] * initial_w)
-                    ymin = int(obj[4] * initial_h)
-                    xmax = int(obj[5] * initial_w)
-                    ymax = int(obj[6] * initial_h)
-                    class_id = int(obj[1])
-                    # Draw box and label\class_id
-                    color = (min(class_id * 12.5, 255), min(class_id * 7, 255), min(class_id * 5, 255))
-                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color, 2)
-                    det_label = labels_map[class_id] if labels_map else str(class_id)
-                    cv2.putText(frame, det_label + ' ' + str(round(obj[2] * 100, 1)) + ' %', (xmin, ymin - 7),
-                                cv2.FONT_HERSHEY_COMPLEX, 0.6, color, 1)
+        #localize the detection of Circle for faster processing
+        if(r != 0):
+            localized_frame = frame[Yloc:Yloc + 4*r, Xloc:Xloc+4*r]
+            cv2.imshow('localized frame', localized_frame)
+        else:
+            localized_frame = frame;
+        
+        # load the image, clone it for output, and then convert it to grayscale
+                
+        output = frame.copy()
+        gray = cv2.cvtColor(localized_frame, cv2.COLOR_BGR2GRAY)
+        
+        # apply GuassianBlur to reduce noise. medianBlur is also added for smoothening, reducing noise.
+        gray = cv2.GaussianBlur(gray,(5,5),0);
+        gray = cv2.medianBlur(gray,5)
+        
+        # Adaptive Guassian Threshold is to detect sharp edges in the Image. For more information Google it.
+        gray = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,3.5)
+        
+        #kernel = np.ones((3,3),np.uint8)
+        gray = cv2.erode(gray,kernel,iterations = 1)
+        # gray = erosion
+        
+        gray = cv2.dilate(gray,kernel,iterations = 1)
+        # gray = dilation
 
-            # Draw performance stats
-            inf_time_message = "Inference time: N\A for async mode" if is_async_mode else \
-                "Inference time: {:.3f} ms".format(det_time * 1000)
-            render_time_message = "OpenCV rendering time: {:.3f} ms".format(render_time * 1000)
-            async_mode_message = "Async mode is on. Processing request {}".format(cur_request_id) if is_async_mode else \
-                "Async mode is off. Processing request {}".format(cur_request_id)
+        # get the size of the final image
+        # img_size = gray.shape
+        # print img_size
+        
+        # detect circles in the image
+        processTimeStart = time.time()
+        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 260, param1=30, param2=100, minRadius=10, maxRadius=0)
+        processTimeEnd = time.time()
+        processTime = processTimeEnd - processTimeStart
+        
+        print(processTime)
+        # print circles
+        
+        # ensure at least some circles were found
+        if circles is not None:
+            # convert the (x, y) coordinates and radius of the circles to integers
+            circles = np.round(circles[0, :]).astype("int")
+            
+            # loop over the (x, y) coordinates and radius of the circles
+            for (x, y, r) in circles:
+                # draw the circle in the output image, then draw a rectangle in the image
+                # corresponding to the center of the circle
+                #cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+                tmpx = Xloc + x
+                tmpy = Yloc + y
+                cv2.rectangle(output, (tmpx - 5, tmpy - 5), (tmpx + 5, tmpy + 5), (0, 128, 255), -1)
+                #time.sleep(0.5)
+                #print ("Column Number: {}".format(x))
+                #print ("Row Number: {}".format(y))
+                #print ("Radius: {}".format(r))
+            
+            x = Xloc + x
+            y = Yloc + y
+        else:
+            y = 0
+            x = 0
+            r = 0
 
-            cv2.putText(frame, inf_time_message, (15, 15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (200, 10, 10), 1)
-            cv2.putText(frame, render_time_message, (15, 30), cv2.FONT_HERSHEY_COMPLEX, 0.5, (10, 10, 200), 1)
-            cv2.putText(frame, async_mode_message, (10, int(initial_h - 20)), cv2.FONT_HERSHEY_COMPLEX, 0.5,
-                        (10, 10, 200), 1)
+        
 
-        #
-        render_start = time.time()
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        cv2.imshow("Detection Results", frame)
-        render_end = time.time()
-        render_time = render_end - render_start
-
-        if is_async_mode:
-            cur_request_id, next_request_id = next_request_id, cur_request_id
-            frame = next_frame
-
-        key = cv2.waitKey(1)
-        if key == 27:
+        # Display the resulting frame
+        #cv2.imshow('gray',gray)
+        cv2.imshow('frame',output)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        if (9 == key):
-            is_async_mode = not is_async_mode
-            log.info("Switched to {} mode".format("async" if is_async_mode else "sync"))
 
+    # When everything done, release the capture
+    cap.release()
     cv2.destroyAllWindows()
-
-
+    
 if __name__ == '__main__':
     sys.exit(main() or 0)
+
